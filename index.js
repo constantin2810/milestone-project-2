8754c41dc15ec7440eacc4b06b825dbb9dce3e91
constantin2810 <czgripcea@gmail.com>
2021-07-21 05:21:47 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // next code will change cards back to face by click on them
+var clickedCard = null;
+
+document.querySelectorAll('.card').forEach(function(card){
+    card.addEventListener('click',onCardClicked);
+});
 
 function onCardClicked(e) {
     const target = e.currentTarget;
-    console.log(target.className);
-    target.className = target.className
-    .replace('back','front front-hidden')
-    .trim();
+   target.getElementsByClassName('back')[0].classList.add('hidden');
+   target.getElementsByClassName('front')[0].classList.remove('hidden');
 
     if (!clickedCard){
         clickedCard = target; 
