9dbd5016fb2c23eabf1585c0cb9a99c0f817609c
constantin2810 <czgripcea@gmail.com>
2021-07-23 15:44:59 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
 // next code will change cards back to face by click on them
 var clickedCard = null;
+var card1 = null;
+var card2 = null;
+var currentScore = 0;
+var moveCounter = 0;
 
 document.querySelectorAll('.card').forEach(function(card){
     card.addEventListener('click',onCardClicked);
 });
 
+
+function checkForMatch(card1, card2){
+    let card1Name = card1.getAttribute('data-card-name');
+    let card2Name = card2.getAttribute('data-card-name');
+
+    if (card1Name.localeCompare(card2Name) === 0 ){
+        console.log('Its a match');
+        card1.removeEventListener('click', onCardClicked);
+        card2.removeEventListener('click', onCardClicked);
+        currentScore = currentScore + 1;
+    }
+    else{
+        console.log('Not a match');
+        unFlipCard(card1);
+        unFlipCard(card2);
+    }
+
+}
+
+function checkForWin(){
+    if (currentScore===8){
+        setTimeout(() => {
+            alert('You won the game');
+        },700);
+    }
+}
+
+
+function flipCard(card){
+    console.log(card)
+    card.getElementsByClassName('front')[0].classList.remove('hidden');
+    card.getElementsByClassName('back')[0].classList.add('hidden');
+}
+
+
+function unFlipCard(card){
+    setTimeout(() => {
+        card.getElementsByClassName('back')[0].classList.remove('hidden');
+        card.getElementsByClassName('front')[0].classList.add('hidden');
+    },700);
+}
+
+function updateMoveCounter(){
+    document.getElementById('moves').innerText = moveCounter;
+}
+
 function onCardClicked(e) {
-   const target = e.currentTarget;
-   if(
-       target === clickedCard ||
-       target.className.includes('done')){
-       return;
-   }
-
-   target.getElementsByClassName('back')[0].classList.add('hidden');
-   target.getElementsByClassName('front')[0].classList.remove('hidden');
-   target.className += 'done';
-
-    if (!clickedCard){
-        // if we haven't clicked a card ,keep track of the card
-        clickedCard = target; 
-    }else if (clickedCard){
-        // if we have already clicked a card , check if the new card matches the old card
-        if(
-            clickedCard.getElementsByClassName('back') !==
-            target.getElementsByClassName('front')
-        ){
-            preventClick = true;
-            setTimeout(() => {
-                clickedCard.className.replace('done','');
-                clickedCard = null;
-                preventClick = false;
-            },500);
-        }else{
-            clickedCard = null;
-        }
+    moveCounter = moveCounter + 1;
+    updateMoveCounter();
+    if (card1 === null){
+        card1 = e.currentTarget;
+        flipCard(card1);
+    }
+    else{
+        card2 = e.currentTarget;
+        flipCard(card2);
+        checkForMatch(card1, card2);
+        checkForWin();
+        card1 = null;
+        card2 = null;
     }
 }
